d1e4f1b9ffe8e3eb605626ee59a8a7293c56bd53
ekatsym <ekatsymeee@gmail.com>
2021-06-09 16:47:56 +0900

    Implemented lazy folds in FCL.VECTOR.

--- src/datatype/vector.lisp
+++ src/datatype/vector.lisp
@@ -107,6 +107,49 @@
   (define-unfoldl+ base-string)
   (define-unfoldl+ simple-base-string))
 
+(defmethod lfoldr (a&$x->x x0 (as vector))
+  (check-type a&$x->x function)
+  (let ((len (length as)))
+    (labels ((rec (i)
+               (if (>= i len)
+                   x0
+                   (funcall a&$x->x (elt as i) (delay (rec (1+ i)))))))
+      (rec 0))))
+
+(defmethod lfoldr+ (a&as&$x->x x0 (as vector))
+  (check-type a&as&$x->x function)
+  (let ((len (length as)))
+    (labels ((rec (i)
+               (if (>= i len)
+                   x0
+                   (funcall a&as&$x->x
+                            (elt as i)
+                            (subseq as (1+ i))
+                            (delay (rec (1+ i)))))))
+      (rec 0))))
+
+(defmethod lfoldl ($x&a->x x0 (as vector))
+  (check-type $x&a->x function)
+  (let ((len (length as)))
+    (labels ((rec (i $x)
+               (declare (optimize (space 3)))
+               (if (>= i len)
+                   (force $x)
+                   (rec (1- i) (delay (funcall $x&a->x (force $x) (elt as i)))))))
+      (rec 0 (delay x0)))))
+
+(defmethod lfoldl+ ($x&a&as->x x0 (as vector))
+  (check-type $x&a&as->x function)
+  (let ((len (length as)))
+    (labels ((rec (i $x)
+               (declare (optimize (space 3)))
+               (if (>= i len)
+                   (force $x)
+                   (rec (1- i)
+                        (delay (funcall $x&a&as->x (force $x) (elt as i) (subseq as (1+ i))))))))
+      (rec 0 (delay x0)))))
+
+
 ;;; MONAD-PLUS
 (macrolet ((define-unit (classname)
              `(defmethod unit ((class (eql ',classname)) a)
