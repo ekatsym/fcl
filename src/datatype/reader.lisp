83c94d6499bc15ebb4b5b861b04d49aa7a80473a
ekatsym <ekatsymeee@gmail.com>
2021-06-09 19:43:09 +0900

    Implemented READER.

--- src/datatype/reader.lisp
+++ src/datatype/reader.lisp
@@ -1,12 +1,48 @@
 (defpackage fcl.reader
   (:nicknames :fcl.data.reader :fcl.rd)
-  (:use :common-lisp :fcl.monad-plus)
+  (:use :common-lisp :fcl.monad)
+  (:import-from
+    :fcl.adata
+    #:defdata)
+  (:import-from
+    :fcl.match
+    #:ematch)
   (:export
+    #:reader #:run-reader #:ask #:local
     #:unit #:fmap #:amap #:mmap
-    #:mlet #:mprogn #:mdo
-    #:define-fmap-by-applicative
-    #:define-fmap-by-monad
-    #:define-amap-by-monad
-    #:guard
-    #:mzero #:mplus #:msum))
+    #:mlet #:mprogn #:mdo))
 (in-package :fcl.reader)
+
+
+;;; Definition
+(defdata reader
+  (%reader function))
+
+(defun run-reader (reader r)
+  (check-type reader reader)
+  (funcall (%reader%0 reader) r))
+
+(defun ask ()
+  (%reader #'identity))
+
+(defun local (r->r a*)
+  (check-type r->r function)
+  (check-type a* reader)
+  (%reader
+    (lambda (r)
+      (run-reader a* (funcall r->r r)))))
+
+
+;;; MONAD
+(defmethod unit ((class (eql 'reader)) a)
+  (%reader (constantly a)))
+
+(define-fmap-by-monad reader)
+
+(define-amap-by-monad reader)
+
+(defmethod mmap (a->b* (a* reader))
+  (check-type a->b* function)
+  (%reader
+    (lambda (r)
+      (run-reader (funcall a->b* (run-reader a* r)) r))))
--- src/package.lisp
+++ src/package.lisp
@@ -51,6 +51,4 @@
     #:lc
     #:vc
     #:ac
-    #:queue
-    #:enqueue
-    #:dequeue))
+    #:reader #:run-reader #:ask #:local))
