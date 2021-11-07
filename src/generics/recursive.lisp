0f3dcfcaab63feec9cb8f190e46c413ec24f502e
ekatsym <ekatsymeee@gmail.com>
2021-11-07 23:41:17 +0900

    Changed the tag type of polynomial from symbol to index.

--- src/generics/recursive.lisp
+++ src/generics/recursive.lisp
@@ -3,7 +3,7 @@
   (:use :common-lisp)
   (:import-from :fcl.adata #:defdata)
   (:import-from :fcl.match #:ematch)
-  (:import-from :fcl.util #:partial #:group)
+  (:import-from :fcl.util #:index #:partial #:group)
   (:export
     #:polynomial #:term
     #:cata #:para #:ana #:apo))
@@ -12,7 +12,7 @@
 
 ;;; Polynomial
 (defdata polynomial
-  (term symbol list))
+  (term index list))
 
 ;;; Catamorphisms
 (defgeneric cata (ax->x a*)
